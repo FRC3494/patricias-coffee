7054ba8bf2f4875a5b492081174e7fe715085538
Edwan Vi <tkdberger@gmail.com>
2017-01-24 19:52:05 -0500

    MemSys

---
+++ JavaBot/src/org/usfirst/frc/team3494/robot/subsystems/MemSys.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team3494.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * hacky way of storing some stuff
+ */
+public class MemSys extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	private double centerX;
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    public void setCenterX(double set) {
+    	this.centerX = set;
+    }
+    public double getCenterX() {
+    	return this.centerX;
+    }
+}
+
