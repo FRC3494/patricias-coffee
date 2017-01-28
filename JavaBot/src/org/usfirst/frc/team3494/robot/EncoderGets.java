214368244a99f186e0ee9516623ab6389ed0da25
Edwan Vi <tkdberger@gmail.com>
2017-01-28 18:34:14 -0500

    Create new enum for types of encoder data

---
+++ JavaBot/src/org/usfirst/frc/team3494/robot/EncoderGets.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team3494.robot;
+
+/**
+ * Enum for storing data "types" to get from encoders.
+ * 
+ * @since 0.0.1
+ * @category Utils
+ */
+public enum EncoderGets {
+	RAWCOUNT, INCHES;
+}
