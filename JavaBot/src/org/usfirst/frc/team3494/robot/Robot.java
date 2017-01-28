7a9f1e34ef6f25466ecd424d718f6c25a6d8d2fe
Edwan Vi <tkdberger@gmail.com>
2017-01-28 15:39:54 -0500

    Prettify code

--- JavaBot/src/org/usfirst/frc/team3494/robot/Robot.java
+++ JavaBot/src/org/usfirst/frc/team3494/robot/Robot.java
@@ -127,8 +127,9 @@ public class Robot extends IterativeRobot {
 		 */
 
 		// schedule the autonomous command (example)
-		if (autonomousCommand != null)
+		if (autonomousCommand != null) {
 			autonomousCommand.start();
+		}
 	}
 
 	/**
