2e2366debc1f759c45b20bdbc8246cc2969ea4d7
Edwan Vi <tkdberger@gmail.com>
2017-01-28 13:23:49 -0500

    Create stop turret command

---
+++ JavaBot/src/org/usfirst/frc/team3494/robot/commands/turret/StopTurret.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team3494.robot.commands.turret;
+
+import org.usfirst.frc.team3494.robot.DriveDirections;
+import org.usfirst.frc.team3494.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * if you're reading this doc because you don't know what this class does you're significantly screwed
+ */
+public class StopTurret extends Command {
+
+    public StopTurret() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.turret);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.turret.turnTurret(DriveDirections.STOP);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
