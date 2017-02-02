bf6a0f01b0eec2163a95cd49ed538ee051d1b414
Edwan Vi <tkdberger@gmail.com>
2017-02-02 18:20:25 -0500

    Source -> format

--- JavaBot/src/org/usfirst/frc/team3494/robot/commands/turret/GoHome.java
+++ JavaBot/src/org/usfirst/frc/team3494/robot/commands/turret/GoHome.java
@@ -14,38 +14,38 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class GoHome extends Command {
 
-    public GoHome() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.turret);
-    }
+	public GoHome() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.turret);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	double location = Robot.turret.getTurretEncDistance(EncoderGets.MILLIMETERS);
-    	if (location > 0) {
-    		Robot.turret.turnTurret(DriveDirections.LEFT);
-    	} else {
-    		Robot.turret.turnTurret(DriveDirections.RIGHT);
-    	}
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		double location = Robot.turret.getTurretEncDistance(EncoderGets.MILLIMETERS);
+		if (location > 0) {
+			Robot.turret.turnTurret(DriveDirections.LEFT);
+		} else {
+			Robot.turret.turnTurret(DriveDirections.RIGHT);
+		}
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return Robot.turret.getTurretEncDistance(EncoderGets.MILLIMETERS) == 0;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return Robot.turret.getTurretEncDistance(EncoderGets.MILLIMETERS) == 0;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.turret.turnTurret(DriveDirections.STOP);
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.turret.turnTurret(DriveDirections.STOP);
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
